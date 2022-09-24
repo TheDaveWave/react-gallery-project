b007722f83759bb4c14a7e7fb2f0e8e3113908af
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 15:01:34 -0500

    added pool.js to modules to connect to database

---
+++ server/modules/pool.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+
+// create a new pool config to connect to database.
+const pool = new pg.Pool({
+    database: 'react-gallery',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
+
+pool.on('error', () => {
+    console.log('Error with database pool', error);
+});
+
+module.exports = pool;
