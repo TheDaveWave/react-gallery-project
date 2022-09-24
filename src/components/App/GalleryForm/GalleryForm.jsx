4402903d89ca1c16e6cc1beb708985517c8d6680
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 16:35:33 -0500

    created skeleton structure for form

--- src/components/App/GalleryForm/GalleryForm.jsx
+++ src/components/App/GalleryForm/GalleryForm.jsx
@@ -1,6 +1,14 @@
 function GalleryForm() {
     return (
-        <h1>Hello there!</h1>
+        <div className="gallery-form">
+            <form>
+                <label htmlFor="path-input">URL </label>
+                <input id="path-input" type="text" required/>
+                <label htmlFor="description-input">Description </label>
+                <input id="description-input" type="text" required/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     );
 }
 
