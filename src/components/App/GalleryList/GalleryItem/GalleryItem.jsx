0ab1199fa34d2da3d58ee22cdf8e285a93ef2f7a
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-25 12:54:01 -0500

    got background to fit the entire page'

--- src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
@@ -41,6 +41,15 @@ function GalleryItem ({item, getGallery}) {
         });
     }
 
+    // icon for a thumbs up.
+    const thumbsUp = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+        <rect width="256" height="256" fill="none"/>
+        <path d="M32,104H80a0,0,0,0,1,0,0V208a0,0,0,0,1,0,0H32a8,8,0,0,1-8-8V112A8,8,0,0,1,32,104Z" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+        <path d="M80,104l40-80a32,32,0,0,1,32,32V80h61.9a15.9,15.9,0,0,1,15.8,18l-12,96a16,16,0,0,1-15.8,14H80" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+    </svg>);
+
+
     return (
         <div className="gallery-item">
             <div className="display" onClick={() => {setToggle(!togglePic)}}>
@@ -49,7 +58,7 @@ function GalleryItem ({item, getGallery}) {
                 <img src={item.path} alt="gallery item"/>}
             </div>
             <div className="display-banner">
-                <button onClick={() => updateLikes(item.id)}>{'\u2713'}</button>
+                <button onClick={() => updateLikes(item.id)}>{thumbsUp}</button>
                 <button onClick={() => deleteItem(item.id)}>X</button>
                 <p>{item.likes === 0 ? 'No people like' : item.likes===1 ? `${item.likes} person likes` : `${item.likes} people like`} this!</p>
             </div>
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -3,14 +3,16 @@ import GalleryItem from './GalleryItem/GalleryItem';
 import './GalleryList.css';
 function GalleryList({gallery, getGallery}) {
     return (
-        <div className='gallery-list'>            
-            {gallery.map( item => (
-                <GalleryItem
-                    key={item.id}
-                    item={item}
-                    getGallery={getGallery}
-                />
-            ))}      
+        <div className="gallery-container">
+            <div className='gallery-list'>            
+                {gallery.map( item => (
+                    <GalleryItem
+                        key={item.id}
+                        item={item}
+                        getGallery={getGallery}
+                    />
+                ))}      
+            </div>
         </div>
     );
 }
